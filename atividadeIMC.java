2bc5a37273b39e585cf7afb3498e1fbd9ea3efc8
Luis E. B. Canal <luiseduardocanal@gmail.com>
2026-03-30 21:17:52 -0300

    alterando a escrita do read genero

--- atividadeIMC.java
+++ atividadeIMC.java
@@ -1,7 +1,7 @@
 void main() {
     IO.println("Cálculo de IMC");
     String nome = IO.readln("Qual seu nome? ");
-    char genero = IO.readln("Informe seu gênero (M: masculino, F: feminino, N: prefio não informar)").charAt(0);
+    char genero = IO.readln("Informe seu gênero (M: masculino, F: feminino, N: prefio não informar): ").charAt(0);
     String alturaString = IO.readln("Qual sua altura (em metros)? ");
     Double altura = Double.parseDouble(alturaString);
     String pesoString = IO.readln("Qual seu peso (em Kg)? ");
